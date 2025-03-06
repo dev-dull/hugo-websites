6eab8ab069427153b088d41aa48f820ce9f86cc7
Alastair Drong <adrong@gmail.com>
2025-03-05 17:25:53 -0800

    add preview script

---
+++ bin/preview.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+errcho () {
+  echo "$@" >&2
+}
+
+here="$(cd $(dirname $0) && pwd)"
+binDir=$(basename "$here")
+
+site=$(gum choose $(ls "$here/.." | grep -v "$binDir" | grep \.))
+
+if [ -d "$here/../$site/themes" ]; then
+  themes=$(ls "$here/../$site/themes")
+  if [ -n "$themes" ]; then
+    theme=$(gum choose "$themes")
+  else
+    errcho "No themes installed"
+  fi
+else
+  errcho "No themes directory detected at '$here/../$site/themes'"
+fi
+
+cd "$here/../$site"
+hugo --theme="$theme" server -D &
+sleep 5
+open http://localhost:1313
+wait $!
